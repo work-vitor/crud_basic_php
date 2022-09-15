e222ce1fb2156283f1d577d643cf1a6667618454
vitor rodriguesm <joa.fggf3.27@gmail.com>
2022-09-15 12:51:36 -0300

    Project init

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <title>Home</title>
+</head>
+<body>
+
+<h1 class="container text-center">Inicio</h1>
+
+<div class="container text-center" style="margin-top: 5%;">
+  <div class="row align-items-start">
+    <div class="col">
+      <a href="views/create.php" style="text-decoration: none ;">Create</a>
+    </div>
+    <div class="col">
+      <a href="views/read.php" style="text-decoration: none ;">Read</a>
+    </div>
+    <div class="col">
+      <a href="views/update.php" style="text-decoration: none ;">Update</a>
+    </div>
+    <div class="col">
+      <a href="views/delete.php" style="text-decoration: none ;">Delete</a>
+    </div>
+  </div>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+</html>
+
